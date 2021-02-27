e93cef4b537b41be3f3d63d7db7691de84f83ffb
yanagi-desu <byang28@g.ecc.u-tokyo.ac.jp>
2021-02-27 10:15:01 +0900

    Initial Commit

---
+++ Wantedly_Test/Wantedly_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wantedly_TestApp.swift
+//  Wantedly_Test
+//
+//  Created by Bofan Yang on 2021/02/27.
+//
+
+import SwiftUI
+
+@main
+struct Wantedly_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
